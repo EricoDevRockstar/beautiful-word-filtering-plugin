ebc1cf229ad35f60cfe9ffd7cc228caaa7c3c008
Eric W. M <ericmwangi0916@gmail.com>
2022-06-18 16:19:46 +0300

    Modified the Index.php file

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+  Plugin Name: Beautiful Word Filtering Plugin
+  Description: Gives you the ability to filter any words you want from your website.
+  Version: 1.1.0
+  Author: Eric W.
+  Author URI: https://www.datiospecio.com/
+*/
